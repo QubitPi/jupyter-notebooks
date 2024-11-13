5f2508330c7be50f0dd02e5a73be7c2b1c9f0a45
Jack <jack20220723@gmail.com>
2024-11-13 09:48:32 +0800

    Centralize rendering logic

---
+++ notebooks/reading-notes/renderer.py
@@ -0,0 +1,20 @@
+def render_arrows_app_json(json_file: str):
+    import json
+    from pyvis.network import Network
+
+    net = Network(notebook=True)
+
+    with open(json_file, 'r') as f:
+        graph = json.load(f)
+        nodes = graph["nodes"]
+        links = graph["relationships"]
+
+    node_map = {}
+    for node in nodes:
+        net.add_node(node["caption"])
+        node_map[node["id"]] = node["caption"]
+
+    for link in links:
+        net.add_edge(node_map[link["fromId"]], node_map[link["toId"]], title=link["type"])
+
+    return net
